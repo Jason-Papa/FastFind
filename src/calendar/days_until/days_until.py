e84f0a6d6c7a404bd0cd113f720fd400c7517a0d
jason <iasonvaspap@gmail.com>
2024-10-13 20:45:53 +0100

    days until additions

--- src/calendar/days_until/days_until.py
+++ src/calendar/days_until/days_until.py
@@ -30,8 +30,36 @@ def days_until(
     typer.echo(difference)
 
 def parse_date_until_string(date_str: str):
+    if date_str.lower() in ["christmas", "xmas", "x-mas"]:
+        date_str = "25-12"
+    if date_str.lower() in ["new-years-eve", "nye", "ny"]:
+        date_str = f"1-1-{datetime.now().year + 1}"
+
+    months_map = {
+        'january': 1,
+        'february': 2,
+        'march': 3,
+        'april': 4,
+        'may': 5,
+        'june': 6,
+        'july': 7,
+        'august': 8,
+        'september': 9,
+        'october': 10,
+        'november': 11,
+        'december': 12
+    }
+    if date_str in months_map:
+        now = datetime.now()
+        date_month = months_map[date_str]
+        year = now.year
+        if now.month > months_map[date_str]:
+            year += 1
+        date_str = f"1-{months_map[date_str]}-{year}"
     try:
         date = date_str.split('-')
+        if len(date) == 2:
+            date.append(datetime.now().year)
         if len(date) != 3:
             raise ValueError
         day, month, year = date
